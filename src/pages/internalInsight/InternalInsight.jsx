49a8fa9a264ec7a79099f5ad75c38a80f7cb7ce6
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-03-26 16:46:22 +0000

    Complete insight page

--- src/pages/internalInsight/InternalInsight.jsx
+++ src/pages/internalInsight/InternalInsight.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { FaArrowUpLong } from "react-icons/fa6";
 
 const InternalInsight = () => {
   return (
@@ -42,20 +43,60 @@ const InternalInsight = () => {
         </div>
 
         <div class="col-span-3 block py-4 px-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-100 ">
-          <div class="flex items-center gap-2">
-            <h5 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-700  ">
-              Internal Insights
-            </h5>
-          </div>
-          <p class="font-normal text-gray-900 dark:text-gray-700 mb-4 ">
-            Ask any question about your business and receive answers and
-            insights in seconds
-          </p>
-          <div class="flex justify-end">
-            <button class="text-white font-semi-bold py-1 px-10 bg-grey-color rounded mt-6 border flex items-center gap-2 shadow">
-              Ask A Question
+          <div className="flex justify-end">
+            <button className="bg-yellow-color hover:bg-yellow-600 text-white font-bold py-3 px-8 rounded mt-6">
+              View Data Source
             </button>
           </div>
+          <div className="flex flex-col items-center text-center p-8">
+            <h1 className="m-4 text-3xl font-bold">Ask Urubytes A Question</h1>
+            <p className="px-32 text-gray-600">
+              Get started extracting bytes-sized nsights from your business
+              data. Just tell us where to look first and you can ask any
+              question about your business.
+            </p>
+          </div>
+
+          <form class="p-2  mx-auto">
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border  rounded-lg   dark:placeholder-gray-400 dark:text-white  "
+                placeholder="Type Here..."
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-grey-color hover:bg-gray-500  font-medium rounded-lg text-sm px-2 py-2  "
+              >
+                <FaArrowUpLong size={20} />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
