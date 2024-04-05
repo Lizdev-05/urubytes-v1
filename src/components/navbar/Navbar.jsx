8c19033bc264b2364d1d5151dae1a941a73249b2
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-05 01:33:54 +0000

    Rework the dashboard

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,33 +9,7 @@ const Navbar = () => {
   return (
     <div className="nav">
       <form className="bg-white">
-        <div className="relative py-8 px-6 flex justify-end gap-12 mr-6 ">
-          {/* <div className="absolute inset-y-0 start-16 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="search"
-            id="default-search"
-            // className="block ps-20 text-sm text-gray-200 border border-gray-300 rounded-full bg-gray-50 dark:bg-gray-150 dark:border-gray-200 dark:placeholder-gray-400 dark:text-white-500 w-11/12 focus:ring-2 focus:outline-none focus:ring-blue-200"
-            class="block  p-4 ps-20 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-300  dark:bg-gray-100  dark:placeholder-gray-400 dark:text-gray-900 w-11/12 focus:ring-2 focus:outline-none focus:ring-blue-200"
-            placeholder="Search"
-            required
-          /> */}
-
+        <div className="relative py-4 px-6 flex justify-end gap-12 mr-6 ">
           <button
             id="dropdownDefaultButton"
             data-dropdown-toggle="dropdown"
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -8,62 +8,90 @@ import ReactPlayer from "react-player";
 
 const Dashboard = () => {
   return (
-    <div className="bg-grey-bg h-screen w-screen">
+    <div className="bg-grey-bg h-screen w-screen  overflow-hidden">
       <Navbar />
-      <div className="p-6 sm:px-16 m-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-300">
-        <div className="">
-          <h1 className="text-3xl font-semibold">Welcome To Urubytes</h1>
-          <div className="px-2 py-2 my-6 border border-gray-200 rounded-lg shadow bg-primary-blue dark:border-gray-300 dark:hover:bg-blue-500">
-            <div className="flex flex-col-reverse sm:flex-row justify-between px-8">
-              <div className="my-8 leading-10 text-white">
-                <h2 className="text-3xl font-semibold mb-4">
-                  Unlock the Power of your Data!
-                </h2>
-                <p className="leading-6">
-                  Know what your business is doing and what you can do better by
-                  asking the right questions
-                </p>
-                <button className="bg-yellow-color hover:bg-yellow-600 text-white font-bold py-1 px-10 rounded mt-6">
-                  Take The Tour
+      <div className="grid grid-cols-1 sm:grid-cols-4 p-2 my-2 overflow-hidden">
+        <div className="sm:col-span-3 py-6 sm:px-16 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-300">
+          <div className="mt-6 ">
+            <h1 className="text-3xl font-semibold">Welcome To Urubytes</h1>
+            <div className="px-2 py-2 my-6 border border-gray-200 rounded-lg shadow bg-primary-blue dark:border-gray-300 dark:hover:bg-blue-500">
+              <div className="flex flex-col-reverse sm:flex-row justify-between px-8">
+                <div className="my-8 leading-10 text-white">
+                  <h2 className="text-3xl font-semibold mb-4">
+                    Unlock the Power of your Data!
+                  </h2>
+                  <p className="leading-6">
+                    Know what your business is doing and what you can do better
+                    by asking the right questions
+                  </p>
+                  <button className="bg-yellow-color hover:bg-yellow-600 text-white font-bold py-1 px-10 rounded mt-6">
+                    Take The Tour
+                  </button>
+                </div>
+                <div>
+                  <img src={dashboardLens} alt="dashboard-lens" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row justify-between py-2 my-8 gap-2">
+            <div className="block py-4 px-12 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-100 transition-transform transform hover:scale-105">
+              <div className="flex items-center gap-2">
+                <img
+                  src={internalInsights}
+                  alt="internal insight"
+                  className="w-12 h-12"
+                />
+                <h5 className="mb-2 text-xl font-bold tracking-tight dark:text-gray-700 flex mt-2">
+                  Get Started{" "}
+                </h5>
+              </div>
+              <p className="font-normal text-gray-900 dark:text-gray-700 mb-4">
+                Start by pointing us to a data source.
+              </p>
+              <div className="flex justify-between">
+                <button className="text-dark font-semi-bold py-2 px-4 rounded-xl mt-6 border border-gray-600 flex items-center gap-2">
+                  <FaPlus />
+                  Add Data Source
                 </button>
               </div>
-              <div>
-                <img src={dashboardLens} alt="dashboard-lens" />
-              </div>
+            </div>
+            <div className="block py-4 px-12 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-100 transition-transform transform hover:scale-105">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=mUMi9VJmMSg&t=73s"
+                height="300"
+                width="300"
+                controls
+                playing
+                title="Urubytes"
+                className="h-full"
+              />
             </div>
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row justify-between py-2 my-6 gap-6">
-          <div className="block py-4 px-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-100 transition-transform transform hover:scale-105">
-            <div className="flex items-center gap-2">
-              <img
-                src={internalInsights}
-                alt="internal insight"
-                className="w-12 h-12"
-              />
-              <h5 className="mb-2 text-xl font-bold tracking-tight dark:text-gray-700 flex mt-2">
-                Get Started{" "}
-              </h5>
+        <div className="block sm:col-span-1 py-4 px-8 bg-white border border-gray-200 rounded-lg shadow dark:border-gray-100">
+          <div>
+            <h2 className=" text-xl font-semibold">Recent Activity</h2>
+            <div className="py-4 px-4 bg-white border border-gray-200 shadow dark:border-gray-10">
+              <h2 className="text-sm font-bold">Total Question Asked</h2>
             </div>
-            <p className="font-normal text-gray-900 dark:text-gray-700 mb-4">
-              Start by pointing us to a data source.
-            </p>
-            <div className="flex justify-between">
-              <button className="text-dark font-semi-bold py-2 px-4 rounded-xl mt-6 border border-gray-600 flex items-center gap-2">
-                <FaPlus />
-                Add Data Source
-              </button>
+            <div className="py-6 px-10 bg-white border border-gray-200 shadow dark:border-gray-10 flex justify-center items-center">
+              <span className="font-bold text-2xl">0</span>
+            </div>
+          </div>
+          <div className="my-6">
+            <div className="py-4 px-4 bg-white border border-gray-200 shadow dark:border-gray-10">
+              <h2 className="text-sm font-bold">Total Sources Connected</h2>
+            </div>
+            <div className="py-6 px-10 bg-white border border-gray-200 shadow dark:border-gray-10 flex justify-center items-center">
+              <span className="font-bold text-2xl">0</span>
             </div>
           </div>
 
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=mUMi9VJmMSg&t=73s"
-            height="200"
-            width="200"
-            controls
-            playing
-            className=" "
-          />
+          <h1 className="mt-4 text-[16px] font-bold">Recent Search</h1>
+          <p className="mt-4 ml-6 font-thin text-gray-500">
+            Nothing here yet...
+          </p>
         </div>
       </div>
     </div>
