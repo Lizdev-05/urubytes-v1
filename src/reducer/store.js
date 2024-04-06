620caf8ebe484e8e118dbf8d1fa8f3140c0ff0e5
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-06 15:03:13 +0000

    Create redux

---
+++ src/reducer/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import registrationReducer from "./reducers";
+
+const store = configureStore({
+  reducer: {
+    registration: registrationReducer,
+  },
+});
+
+export default store;
