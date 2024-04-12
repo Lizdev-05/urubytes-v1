caf490a5ffdd25b46fbe38fad2a2ef538a8da555
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-12 19:12:38 +0000

    Update the store so that the reg is not persisted

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,26 +1,51 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import registrationReducer from "./registrationReducer";
+// import loginReducer from "./loginReducer";
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
+
+// const rootReducer = combineReducers({
+//   registration: registrationReducer,
+//   login: loginReducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+// });
+
+// let persistor = persistStore(store);
+
+// export { store, persistor };
+
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import registrationReducer from "./registrationReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import loginReducer from "./loginReducer";
+import registrationReducer from "./registrationReducer";
 
 const persistConfig = {
-  key: "root",
+  key: "login",
   storage,
 };
 
-const rootReducer = combineReducers({
+const rootReducer = {
+  login: persistReducer(persistConfig, loginReducer),
   registration: registrationReducer,
-  login: loginReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+};
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
 });
 
-let persistor = persistStore(store);
+const persistor = persistStore(store);
 
 export { store, persistor };
