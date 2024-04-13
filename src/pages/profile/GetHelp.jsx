a0a849a16658097f9e01f92536aeb33a80e90cae
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-13 00:33:29 +0000

    Add env file

--- src/pages/profile/GetHelp.jsx
+++ src/pages/profile/GetHelp.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import emailjs from "emailjs-com";
 
 const GetHelp = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="bg-grey-bg h-screen w-screen overflow-hidden internal">
       <Navbar />
