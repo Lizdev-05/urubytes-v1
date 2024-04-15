b6473d3b5e3aaf14398963e0cecced49a22fbfbf
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-15 22:13:14 +0000

    Update the library endpoint

--- src/pages/internalInsight/InternalInsight.jsx
+++ src/pages/internalInsight/InternalInsight.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { FaArrowUpLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
