3150ea64edb6908cd4ebcb86011f27b4107018c9
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-22 16:19:08 +0000

    Use connected sources to return the user

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,135 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import NewUser from "./NewUser";
-// import ReturningUser from "./ReturningUser";
-// import { useSelector, useDispatch } from "react-redux";
-// import Spinner from "../../components/Spinner";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-//   const [userQueries, setUserQueries] = useState([]);
-//   const [data, setData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const token = useSelector((state) => state.login.token);
-
-//   useEffect(() => {
-//     fetch(
-//       "https://urubytes-backend-v2-r6wnv.ondigitalocean.app/auxi/dashboard/",
-//       {
-//         headers: {
-//           Authorization: `Token ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setUserQueries(data.aggregates.userQueries);
-//         setData(data);
-//         setIsLoading(false);
-//         console.log("User queries:", data.aggregates.userQueries);
-//       });
-//   }, [dispatch]);
-
-//   if (isLoading) {
-//     <Spinner />;
-//   }
-
-//   return !userQueries || userQueries.length === 0 ? (
-//     <NewUser />
-//   ) : (
-//     <ReturningUser userQueries={userQueries} data={data} />
-//   );
-// };
-
-// export default Dashboard;
-
-// import React, { useEffect, useState } from "react";
-// import NewUser from "./NewUser";
-// import ReturningUser from "./ReturningUser";
-// import { useSelector, useDispatch } from "react-redux";
-// import Spinner from "../../components/Spinner";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-//   const [userQueries, setUserQueries] = useState([]);
-//   const [data, setData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const token = useSelector((state) => state.login.token);
-
-//   useEffect(() => {
-//     fetch(
-//       "https://urubytes-backend-v2-r6wnv.ondigitalocean.app/auxi/dashboard/",
-//       {
-//         headers: {
-//           Authorization: `Token ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setUserQueries(data.aggregates.userQueries);
-//         setData(data);
-//         setIsLoading(false);
-//         console.log("User queries:", data.aggregates.userQueries);
-//       });
-//   }, [dispatch]);
-
-//   if (isLoading) {
-//     return <Spinner />;
-//   }
-
-//   return !userQueries || userQueries.length === 0 ? (
-//     <NewUser />
-//   ) : (
-//     <ReturningUser userQueries={userQueries} data={data} />
-//   );
-// };
-
-// export default Dashboard;
-
-// import React, { useEffect, useState } from "react";
-// import NewUser from "./NewUser";
-// import ReturningUser from "./ReturningUser";
-// import { useSelector, useDispatch } from "react-redux";
-// import Spinner from "../../components/Spinner";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-//   const [userQueries, setUserQueries] = useState([]);
-//   const [data, setData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const token = useSelector((state) => state.login.token);
-
-//   useEffect(() => {
-//     fetch(
-//       "https://urubytes-backend-v2-r6wnv.ondigitalocean.app/auxi/dashboard/",
-//       {
-//         headers: {
-//           Authorization: `Token ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setUserQueries(data.aggregates.userQueries);
-//         setData(data);
-//         setIsLoading(false);
-//         console.log("User queries:", data.aggregates.userQueries);
-//       });
-//   }, [dispatch]);
-
-//   if (isLoading) {
-//     return <Spinner />;
-//   }
-
-//   return !userQueries || userQueries.length === 0 ? (
-//     <NewUser />
-//   ) : (
-//     <ReturningUser userQueries={userQueries} data={data} />
-//   );
-// };
-
-// export default Dashboard;
-
 import React, { useEffect, useState } from "react";
 import NewUser from "./NewUser";
 import ReturningUser from "./ReturningUser";
@@ -165,7 +33,12 @@ const Dashboard = () => {
     <Spinner />;
   }
 
-  return !userQueries || userQueries.length === 0 ? (
+  // return !userQueries || userQueries.length === 0 ? (
+  //   <NewUser />
+  // ) : (
+  //   <ReturningUser userQueries={userQueries} data={data} />
+  // );
+  return !connectedSources || connectedSources.length === 0 ? (
     <NewUser />
   ) : (
     <ReturningUser userQueries={userQueries} data={data} />
@@ -173,52 +46,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-// import React, { useEffect, useState, useMemo } from "react";
-// import NewUser from "./NewUser";
-// import ReturningUser from "./ReturningUser";
-// import { useSelector, useDispatch } from "react-redux";
-// import Spinner from "../../components/Spinner";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-//   const [userQueries, setUserQueries] = useState([]);
-//   const [data, setData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const token = useSelector((state) => state.login.token);
-
-//   useEffect(() => {
-//     fetch(
-//       "https://urubytes-backend-v2-r6wnv.ondigitalocean.app/auxi/dashboard/",
-//       {
-//         headers: {
-//           Authorization: `Token ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setUserQueries(data.aggregates.userQueries);
-//         setData(data);
-//         setIsLoading(false);
-//         console.log("User queries:", data.aggregates.userQueries);
-//       });
-//   }, [dispatch]);
-
-//   const ReturningUserMemo = useMemo(
-//     () => <ReturningUser userQueries={userQueries} data={data} />,
-//     [userQueries, data]
-//   );
-
-//   if (isLoading) {
-//     return <Spinner />;
-//   }
-
-//   return !userQueries || userQueries.length === 0 ? (
-//     <NewUser />
-//   ) : (
-//     <ReturningUser userQueries={userQueries} data={data} />
-//   );
-// };
-
-// export default Dashboard;
