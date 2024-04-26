ee93239479c511c1d5a2eff5458d60e91aaa629d
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-04-26 16:07:55 +0000

    Update the stop search logic

--- src/pages/internalInsight/InternalInsight.jsx
+++ src/pages/internalInsight/InternalInsight.jsx
@@ -675,7 +675,133 @@ const InternalInsight = () => {
           </div>
 
           <div className="sm:col-span-3 block py-4 px-8 bg-white border border-gray-200 rounded-lg shadow dark:border-gray-100 mainInternal">
-            {/* Your main content here */}
+            <div className="flex justify-between items-center mb-4">
+              <div className="flex items-center justify-between">
+                <span
+                  className={`mr-3 ${
+                    mode === "internal" ? "text-dark" : "text-gray-500"
+                  }`}
+                >
+                  Internal Insight
+                </span>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={mode === "external"}
+                    onChange={handleToggle}
+                  />
+                  <span className="slider round"></span>
+                </label>
+                <span
+                  className={`ml-3 ${
+                    mode === "external" ? "text-dark" : "text-gray-500"
+                  }`}
+                >
+                  External Insight
+                </span>
+              </div>
+              <Link
+                to="/addSource"
+                className="border border-transparent rounded-lg flex items-center bg-yellow-color hover:bg-yellow-600"
+              >
+                <button className="text-white font-semi-bold py-2 px-8 flex items-center gap-2">
+                  <FaPlus />
+                  Add Source
+                </button>
+              </Link>
+            </div>
+            <div className="flex flex-col items-center text-center p-8 internalText">
+              <h1 className="m-4 text-3xl font-bold">
+                Ask Urubytes A Question
+              </h1>
+              <p className="px-32 text-gray-600">
+                Get started extracting bytes-sized insights from your business
+                data. Just tell us where to look first and you can ask any
+                question about your business.
+              </p>
+            </div>
+
+            <form onSubmit={handleSubmit} className="p-2 mx-auto relative">
+              <label
+                htmlFor="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-4 ps-10 text-sm text-blue-500 border rounded-lg dark:placeholder-gray-400 dark:text-gray-500"
+                  placeholder="Type Here..."
+                  required
+                  value={query}
+                  onChange={handleChange}
+                />
+                {/* <button
+                  type="submit"
+                  className="text-white absolute end-2.5 bottom-2.5 bg-grey-color hover:bg-gray-500 font-medium rounded-lg text-sm px-2 py-2"
+                >
+                  <FaArrowUpLong size={20} />
+                </button> */}
+                <button
+                  type="submit"
+                  className="text-white absolute end-2.5 bottom-2.5 bg-grey-color hover:bg-gray-500 font-medium rounded-lg text-sm px-2 py-2"
+                  onClick={loading ? handleStop : handleSubmit}
+                >
+                  {loading ? (
+                    <CiStop1 size={20} />
+                  ) : (
+                    <FaArrowUpLong size={20} />
+                  )}
+                </button>
+              </div>
+              {loading && (
+                <div className="absolute bottom-0 left-0 right-0 top-[5rem] flex items-center space-x-2 ">
+                  <span>Urubytes is thinking </span>
+                  <BeatLoader color={"#123abc"} loading={loading} size={15} />
+                </div>
+              )}
+            </form>
+
+            {feedback && (
+              <div className="p-4 my-4 bg-gray-100 rounded-lg shadow-md ">
+                <div className="text-gray-600 mb-4 insights">
+                  <Markdown remarkPlugins={[remarkGfm]}>
+                    {feedback.insights}
+                  </Markdown>
+                </div>
+
+                <p>
+                  {Object.entries(feedback.metadata).map(([key, value]) => (
+                    <div key={key}>
+                      Source:
+                      <span className="text-primary-blue">
+                        {value.file_name}
+                      </span>
+                    </div>
+                  ))}
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
